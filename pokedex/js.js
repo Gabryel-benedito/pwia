7316d8b7f7795aba8684345a9b3ccb4d46a8fb5c
alunoEtec <aula@etec.com>
2025-06-04 22:42:12 -0300

    inicio pokedes

---
+++ pokedex/js.js
@@ -0,0 +1,23 @@
+const input = document.getElementById("pk");
+const but = document.getElementById("but");
+const nome = document.getElementById("nome");
+const img = document.getElementById("img");
+
+function gerarPokemon(){
+    const url = "https://pokeapi.co/api/v2/pokemon/" + input.value
+    fetch(url)
+    .then(response => response.json())
+    .then((dados) => {
+
+        nome.value = dados.name
+        img.src = dados.sprites.front_default
+        img.style.height = "200px"
+        img.style.width = "200px"
+    })
+
+
+    console.log("funcionou")
+}
+
+
+but.addEventListener("click", gerarPokemon)
