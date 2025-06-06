2d038beed811c17dbd1e4b1fe504ccbf35135d25
alunoEtec <aula@etec.com>
2025-06-06 20:46:00 -0300

    mexer no layout

--- pokedex/js.js
+++ pokedex/js.js
@@ -8,17 +8,13 @@ const gmax = document.getElementById("gmax");
 const tipo1 = document.getElementById("tipo1");
 const tipo2 = document.getElementById("tipo2");
 const conteiner = document.getElementById("cont");
+const pedra = document.getElementById("pedra");
 
 
 function gerarPokemon() {
     const url = "https://pokeapi.co/api/v2/pokemon/" + input.value.toLowerCase();
     fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                console.error("Pokémon não encontrado"); return
-            }
-            return response.json();
-        })
+        .then(response => response.json())
 
         .then((dados) => {
 
@@ -28,6 +24,7 @@ function gerarPokemon() {
             img.style.width = "200px"
             tipo1.value = dados.types[0].type.name
             tipo2.value = dados.types[1] ? dados.types[1].type.name : "Nenhum"
+            pedra.src = dados.sprites.other.home.front_default
 
         })
 
@@ -52,9 +49,31 @@ function evo() {
 }
 
 
-const mouse = {
-    pressiona: false,
-    posicaoInicial: 0
+function evox() {
+    const url = "https://pokeapi.co/api/v2/pokemon/" + input.value.toLowerCase() + "-mega-x"
+    fetch(url)
+        .then(response => response.json())
+        .then((dados) => {
+
+
+            mega.src = dados.sprites.other.showdown.front_default
+            mega.style.height = "200px"
+            mega.style.width = "200px"
+        })
+}
+
+
+function evoy() {
+    const url = "https://pokeapi.co/api/v2/pokemon/" + input.value.toLowerCase() + "-mega-y"
+    fetch(url)
+        .then(response => response.json())
+        .then((dados) => {
+
+
+            mega.src = dados.sprites.other.showdown.front_default
+            mega.style.height = "200px"
+            mega.style.width = "200px"
+        })
 }
 
 
@@ -187,13 +206,110 @@ function mudaMega(direcao) {
 
             transicao(imagens[indiceAtual]);
         });
+}
+
+function mudaMegaX(direcao) {
+    const url = "https://pokeapi.co/api/v2/pokemon/" + input.value.toLowerCase() + "-mega-x";
+    fetch(url)
+        .then(response => response.json())
+        .then((dados) => {
+            let imagens = [
+                dados.sprites.other.showdown.front_default,
+                dados.sprites.other.showdown.back_default,
+                dados.sprites.other.showdown.front_shiny,
+                dados.sprites.other.showdown.back_shiny,
+            ];
 
 
+            function transicao(Src) {
+                mega.style.transition = "opacity 0.3s ease";
+                mega.style.opacity = 0;
+                setTimeout(() => {
+                    mega.src = Src;
+                    mega.style.opacity = 1;
+                }, 300);
+            }
+
+
+            mega.style.height = "200px";
+            mega.style.width = "200px";
+            // Verifica o índice da imagem atual
+            let indiceAtual = imagens.indexOf(mega.src);
+            if (indiceAtual === -1) {
+                // Se a imagem atual não estiver no array, comece com a primeira imagem
+                indiceAtual = 0;
+            }
+            // Avança ou retrocede na lista de imagens
+            if (direcao < 0) {
+                indiceAtual = (indiceAtual + 1) % imagens.length; // Avança para a próxima imagem
+            } else {
+                indiceAtual = (indiceAtual - 1 + imagens.length) % imagens.length; // Volta para a imagem anterior
+            }
+            mega.src = imagens[indiceAtual];
+
+
+
+            transicao(imagens[indiceAtual]);
+        });
 }
 
 
+function mudaMegaY(direcao) {
+    const url = "https://pokeapi.co/api/v2/pokemon/" + input.value.toLowerCase() + "-mega-y";
+    fetch(url)
+        .then(response => response.json())
+        .then((dados) => {
+            let imagens = [
+                dados.sprites.other.showdown.front_default,
+                dados.sprites.other.showdown.back_default,
+                dados.sprites.other.showdown.front_shiny,
+                dados.sprites.other.showdown.back_shiny,
+            ];
 
+
+            function transicao(Src) {
+                mega.style.transition = "opacity 0.3s ease";
+                mega.style.opacity = 0;
+                setTimeout(() => {
+                    mega.src = Src;
+                    mega.style.opacity = 1;
+                }, 300);
+            }
+
+
+            mega.style.height = "200px";
+            mega.style.width = "200px";
+            // Verifica o índice da imagem atual
+            let indiceAtual = imagens.indexOf(mega.src);
+            if (indiceAtual === -1) {
+                // Se a imagem atual não estiver no array, comece com a primeira imagem
+                indiceAtual = 0;
+            }
+            // Avança ou retrocede na lista de imagens
+            if (direcao < 0) {
+                indiceAtual = (indiceAtual + 1) % imagens.length; // Avança para a próxima imagem
+            } else {
+                indiceAtual = (indiceAtual - 1 + imagens.length) % imagens.length; // Volta para a imagem anterior
+            }
+            mega.src = imagens[indiceAtual];
+
+
+
+            transicao(imagens[indiceAtual]);
+        });
+}
+
+
+input.addEventListener("keypress", (evento) => {
+    if(evento.key === "Enter"){
+        gerarPokemon()
+    }
+})
 but.addEventListener("click", gerarPokemon)
 Butmega.addEventListener("click", evo)
+Butmega.addEventListener("click", evox)
+Butmega.addEventListener("dblclick", evoy)
 img.addEventListener("click", () => mudaImagem(-1))
 mega.addEventListener("click", () => mudaMega(-1))
+mega.addEventListener("click", () => mudaMegaX(-1))
+mega.addEventListener("dblclick", () => mudaMegaY(-1))
